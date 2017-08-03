cc0adb4e2b2264e6c24cbfc854f29d8f24046f3d
Emilio Dalla Torre <emiliodallatorre12@live.com>
2017-08-03 11:28:46 +0200

    Changed some methods to make the app more lightweight.

--- app/src/main/java/it/emiliodallatorre/parserlatino/MainActivity.java
+++ app/src/main/java/it/emiliodallatorre/parserlatino/MainActivity.java
@@ -1,63 +1,25 @@
 package it.emiliodallatorre.parserlatino;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Context context;
     EditText search;
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void Main() {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         search = (EditText) findViewById(R.id.search);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String ricerca = search.getText().toString();
-                Intent intent = new Intent(context, trova.class);
-                intent.putExtra("ricerca", ricerca);
-                context.startActivity(intent);
+                trova.trova(ricerca);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/it/emiliodallatorre/parserlatino/trova.java
+++ app/src/main/java/it/emiliodallatorre/parserlatino/trova.java
@@ -1,9 +1,12 @@
 package it.emiliodallatorre.parserlatino;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -14,21 +17,20 @@ import java.io.IOException;
 /**
  * Created by Emilio Dalla Torre on 21/07/2017.
  */
-public class trova extends Activity {
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    private Context context;
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        final String ricerca = getIntent().getStringExtra("ricerca");
+public class trova extends AppCompatActivity {
+    public static void trova(String parola) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        Document document = null;
-        TextView art = (TextView) findViewById(R.id.result);
         try {
-            document = Jsoup.connect("http://www.dizionario-latino.com/dizionario-latino-italiano.php?parola=" + ricerca).userAgent("Mozilla").get();
-            String divs = document.select("#myth").text();
-            art.setText(divs);
+            Document document = Jsoup.connect("http://www.dizionario-latino.com/dizionario-latino-italiano.php?parola=" + parola).userAgent("Mozilla").get();
+            String nominativo = document.select("#myth").text();
+            trova.gui.(nominativo);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public void gui(String risposta) {
+        TextView responso = (TextView) findViewById(R.id.result);
+        responso.setText(risposta);
     }
 }
